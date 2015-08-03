6b453776e1617cb7aca66646828cb8cedcbfc8ba
jirisemecky <jiri.semecky@gmail.com>
2015-08-03 02:48:42 +0200

    Moving class Instruction to a separate file.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # milight-java
 Java API for Milight (LimitlessLED) light bulbs.
+
+Current implementation only supports instruction set for RGB+W bulbs.
---
+++ src/cz/semecky/milight/Instruction.java
@@ -0,0 +1,38 @@
+package cz.semecky.milight;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/** Single instruction of the command that can be sent to Milight bridge. */
+public class Instruction {
+    private final byte[] data;
+
+    public Instruction(byte... data) {
+        this.data = data;
+    }
+
+    public Instruction(String data) {
+        String[] bytes = data.split(" ");
+        this.data = new byte[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            this.data[i] = (byte) (Integer.parseInt(bytes[i],16) & 0xFF);
+        }
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public int getLength() {
+        return data.length;
+    }
+
+    void send(InetAddress inetAddress, int port) throws IOException, InterruptedException {
+        DatagramPacket packet = new DatagramPacket(data, data.length, inetAddress, port);
+        DatagramSocket socket = new DatagramSocket();
+        socket.send(packet);
+        socket.close();
+    }
+}
