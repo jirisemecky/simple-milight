390dc22655c60db84b5655c18ecd95dd2868df77
jirisemecky <jiri.semecky@gmail.com>
2016-02-12 23:54:04 +0100

    Blick turns on the lights again.

--- java/cz/semecky/simplemilight/tools/BlickAllAddresses.java
+++ java/cz/semecky/simplemilight/tools/BlickAllAddresses.java
@@ -10,7 +10,12 @@ public class BlickAllAddresses {
         String address = "192.168.0.";
         System.out.print("Starting ");
         for (int i = 0; i < 255; i++) {
-            RGBW.ZONE_4.off().send(address + i);
+            RGBW.ZONE_1.off().send(address + i);
+            System.out.print(".");
+        }
+        Thread.sleep(3000);
+        for (int i = 0; i < 255; i++) {
+            RGBW.ZONE_1.on().send(address + i);
             System.out.print(".");
         }
         System.out.println(" done.");
